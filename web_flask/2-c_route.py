3aa0e5a64581ad9f15539400ac64e8f73d29aaee
ONTHEWAY <ONTHEWAY@gmail.com>
2024-06-28 10:29:50 +0300

    altering 10:30 AM

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,36 +1,35 @@
 #!/usr/bin/python3
 """Starts a Flask web application.
+
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /: Displays 'Hello HBNB!'.
+    /hbnb: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
 
 app = Flask(__name__)
-app.url_map.strict_slashes = False
 
 
-@app.route('/')
-def home():
-    """
-    Displays 'Hello HBNB!'.
-    """
-    return 'Hello HBNB!'
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!'."""
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb')
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """
-    Displays 'HBNB'.
-    """
+    """Displays 'HBNB'."""
     return "HBNB"
 
 
-@app.route('/c/<text>')
-def c_with_params(text):
-    """
-    Displays 'C' followed by the value of <text>.
-    """
-    text_no_underscore = text.replace('_', ' ')
-    return "C {}".format(text_no_underscore)
+@app.route("/c/<text>", strict_slashes=False)
+def c(text):
+    """Displays 'C' followed by the value of <text>."""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,30 +1,32 @@
-#!/usr/bin/python
-"""
+#!/usr/bin/python3
+"""Starts a Flask web application.
 
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /cities_by_states: HTML page with a list of all states and related cities.
 """
-from flask import Flask, render_template
-from models.state import State
 from models import storage
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
-@app.teardown_appcontext
-def teardown(exception):
-    """
+@app.route("/cities_by_states", strict_slashes=False)
+def cities_by_states():
+    """Displays an HTML page with a list of all states and related cities.
 
+    States/cities are sorted by name.
     """
+    states = storage.all("State")
+    return render_template("8-cities_by_states.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    """Remove the current SQLAlchemy session."""
     storage.close()
 
 
-@app.route('/cities_by_states', strict_slashes=False)
-def states_list():
-    """
-    State list
-    """
-    states = storage.all(State)
-    return render_template('8-cities_by_states.html', states=states)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
